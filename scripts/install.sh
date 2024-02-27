d994b727217e2cc5338a83dd4fa8e7827361adf1
Pale Kiwi <contact@palekiwi.com>
2024-02-27 12:01:19 +0800

    [dev]: add scripts/install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+dir=~/.venv/ansible
+
+if [ ! -d $dir ]; then
+    echo "Creating venv dir..."
+    mkdir -p $dir
+fi
+
+echo "Creating python venv in ${dir}..."
+python -m venv $dir
+
+echo "Activating venv..."
+source $dir/bin/activate
+
+echo "Installing ansible..."
+pip install ansible
+
+echo "Installing roles from requirements..."
+ansible-galaxy install -r requirements.yml
+
+echo "Building dev image..."
+ansible-playbook playbooks/dev_image.yml
+
+echo "Creating dev container..."
+ansible-playbook playbooks/dev_create.yml
+
+echo -e "\nDONE\n"
+
+echo "To provision, enter the container and run:"
+echo -e "\n"
+
+echo "cd $(pwd)"
+echo "ansible-playbook playbooks/dev.yml"
+echo -e "\n"
